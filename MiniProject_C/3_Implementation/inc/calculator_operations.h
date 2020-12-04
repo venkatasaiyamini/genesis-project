76f5f82279ab624d10c979fea4cd4d4d58cb03c3
venkatasaiyamini <69252693+venkatasaiyamini@users.noreply.github.com>
2020-12-03 22:29:09 -0800

    Update calculator_operations.h

--- MiniProject_C/3_Implementation/inc/calculator_operations.h
+++ MiniProject_C/3_Implementation/inc/calculator_operations.h
@@ -3,45 +3,28 @@
 * Calculator application with 4 mathematical operations
 *
 */
-#ifndef __CALCULATOR_OPERATIONS_H__
-#define __CALCULATOR_OPERATIONS_H__
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<limits.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdio_ext.h>
+struct emp{
+	int emp_id;
+        char name[50]; 
+        int age; 
+	char address[50];
+        float salary;
+	int year_of_experience;
+	char post[50];
+	
+};
 
-/**
-*  adds the operand1 and operand2 and returns the result
-* @param[in] operand1 
-* @param[in] operand2 
-* @return Result of operand1 + operand2
-*/
-int add(int operand1, int operand2);
+struct emp e;
 
-/**
-*  subtracts the operand1, operand2 and returns the result
-* @param[in] operand1 
-* @param[in] operand2 
-* @return Result of operand1 - operand2
-*/
-int subtract(int operand1, int operand2);
-
-/**
-*  multiply the operand1, operand2 and returns the result
-* @param[in] operand1 
-* @param[in] operand2 
-* @return Result of operand1 * operand2
-*/
-int multiply(int operand1, int operand2);
-
-
-/**
-* divides the operand1 by operand2 and returns the result 
-* @param[in] operand1 
-* @param[in] operand2 
-* @return integer value of the operand1 / operand2
-* @note returns 0 for divide by 0 error
-*/
-int divide(int operand1, int operand2);
-
-#endif  /* #define __CALCULATOR_OPERATIONS_H__ */
+void mainmenu();
+void add();
+int already(int);
+void display();
+void delete();
+void modify();
+void search();
